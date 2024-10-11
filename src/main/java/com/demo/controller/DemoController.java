96623b6106fcdb33d0f23f0a469fc972c2b3beb2
the-andrewblackman <andrewronaldblackman@gmail.com>
2024-10-11 09:27:25 -0700

    added service logic for descent

--- src/main/java/com/demo/controller/DemoController.java
+++ src/main/java/com/demo/controller/DemoController.java
@@ -1,4 +1,16 @@
 package com.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DemoController {
+
+    @GetMapping ("/directions")
+    public String directions(){
+
+        return "";
+    }
+
 }
