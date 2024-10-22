30bf21ebe31193096e722cc7b477a7ff1c9db775
the-andrewblackman <andrewronaldblackman@gmail.com>
2024-10-21 18:21:26 -0700

    optimized imports

--- src/main/java/com/demo/controller/DemoController.java
+++ src/main/java/com/demo/controller/DemoController.java
@@ -4,10 +4,7 @@ import com.demo.service.PyramidDescentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 public class DemoController {
--- src/main/java/com/demo/entity/PyramidData.java
+++ src/main/java/com/demo/entity/PyramidData.java
@@ -5,15 +5,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PyramidData {
-         public StringBuilder directions;
-         public StringBuilder calculatedDirections;
-         public int target;
-         public int calculatedTargetProduct;
-         public int fileIndex = 0;
-         public int index = 0;
-         public boolean switcher = true;
-         public List<Integer> targetProductList = new ArrayList();
-         public List<int[][]> listOfFiles = new ArrayList<>();
-         public List<Path> files;
+    public StringBuilder directions;
+    public StringBuilder calculatedDirections;
+    public int target;
+    public int calculatedTargetProduct;
+    public int fileIndex = 0;
+    public int index = 0;
+    public boolean switcher = true;
+    public List<Integer> targetProductList = new ArrayList();
+    public List<int[][]> listOfFiles = new ArrayList<>();
+    public List<Path> files;
     public int[][] pyramidArr;
 }
